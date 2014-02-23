c02a7df37542171dd3a3744d3f49a7a0f779a483
cezio <cezio@thelirium.net>
2014-02-23 20:05:24 +0100

    base functions: isFile, isDir

--- imgtit/imgop.go
+++ imgtit/imgop.go
@@ -24,15 +24,36 @@ type DestinationImgParams struct {
 
 };
 
+/* Return true if provided path is a directory. Any other case or error will return false.
+*/
 func isDir(path string) bool{
+    var f, err = os.Open(path);
+    if err != nil{
+        return false;
+    }
+    var fi,errf = f.Stat();
+    if errf != nil{
+        return false;
+    }
+    return fi.IsDir();
 
 }
 
+/* Return true if provided path is a regular file. Any other case or error will return false.
+*/
 func isFile(path string) bool {
-
+    var f, err = os.Open(path);
+    if err != nil{
+        return false;
+    }
+    var fi, errf = f.Stat();
+    if errf != nil{
+        return false;
+    }
+    return fi.Mode().IsRegular();
 }
 
 func Run(opts Options) (bool, error) {
     // check if InputDir exists and is directory
-
+    return true, nil;
 };
